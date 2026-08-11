6e83eb60a85645cd17ed690616a3194de98f9e30
ohganed <64299676+ohganed@users.noreply.github.com>
2026-08-11 22:06:55 +0900

    Cache Sleep Trace storage layer

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = "sleep-trace-cache-v1";
+const CACHE_NAME = "sleep-trace-cache-v2";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./deployment.config.json",
+  "./storage-layer.js",
   "./icons/icon-180.svg",
   "./icons/icon-192.svg",
   "./icons/icon-512.svg"
